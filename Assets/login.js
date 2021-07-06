c533e2b9266ac59e317457c766b8cc734c96234d
Zaky Jamaluddin <86007103+zakyjamaluddin1812@users.noreply.github.com>
2021-07-06 19:31:05 +0700

    ubah url

--- Assets/login.js
+++ Assets/login.js
@@ -1,11 +1,11 @@
 function login() {
     const sandi = document.querySelector("#sandi").value;
     if (sandi == "admin") {
-        window.location.replace("D:/UNUGIRI/TugasAkhirAnalisisDanPerancanganSistem/Admin/admin.html")
+        window.location.replace("https://raw.githack.com/zakyjamaluddin1812/APS-Unugiri/main/Admin/admin.html")
     } else if (sandi == "santri") {
-        window.location.replace("D:/UNUGIRI/TugasAkhirAnalisisDanPerancanganSistem/Santri/santri.html")
+        window.location.replace("https://raw.githack.com/zakyjamaluddin1812/APS-Unugiri/main/Santri/santri.html")
     } else if (sandi == "ustadz") {
-        window.location.replace("D:/UNUGIRI/TugasAkhirAnalisisDanPerancanganSistem/Guru/guru.html")
+        window.location.replace("https://raw.githack.com/zakyjamaluddin1812/APS-Unugiri/main/Guru/guru.html")
     } else {
         const alert = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>Holy guacamole!</strong> You should check in on some of those fields below.
